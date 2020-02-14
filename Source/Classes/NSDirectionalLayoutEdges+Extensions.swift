fe7dcb4a02a4c9f0ba72ae761a8b76b81de4855e
Stefan Herold <stefan.herold@gmail.com>
2020-02-14 14:23:06 +0100

    Fix copyrights

--- Source/Classes/NSDirectionalLayoutEdges+Extensions.swift
+++ Source/Classes/NSDirectionalLayoutEdges+Extensions.swift
@@ -3,7 +3,7 @@
 //  Source
 //
 //  Created by Stefan Herold on 07.08.19.
-//  Copyright © 2019 CodingCobra. All rights reserved.
+//  Copyright © 2020 Stefan Herold. All rights reserved.
 //
 
 import UIKit
