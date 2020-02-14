d8f9376e1358a5e4c00002f9e5ba66adc2e136af
Stefan Herold <stefan.herold@gmail.com>
2020-02-14 14:01:19 +0100

    Replace fatalError by preconditionFailure
    https://lapcatsoftware.com/articles/fatalError.html

--- Example/Source/MyConfigurableCell.swift
+++ Example/Source/MyConfigurableCell.swift
@@ -23,7 +23,7 @@ class MyConfigurableCell: UITableViewCell, Reusable {
 
     @available(*, unavailable, message: "init(coder:) has not been implemented")
     required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        preconditionFailure("init(coder:) has not been implemented")
     }
 
     private func setupLayoutConstraints() {
